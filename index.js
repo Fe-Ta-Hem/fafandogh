ce5e04b66379a85c02e1ac34aa6441048767d42b
Fe-Ta-Hem <fatemeh.nikseresht74@gmail.com>
2024-09-06 21:19:55 +0330

    personal practice

---
+++ index.js
@@ -0,0 +1,16 @@
+function myFirstOne (){
+    document.getElementById("check").innerHTML="check done";
+}
+function mySecOne (){
+    document.getElementById("demo").innerHTML="TSCHUS";
+}
+document.write(5 + 6);
+window.alert(5+9);
+console.log(10+21);
+let text1="A"; 
+let text2="B";
+let result = text1<text2 ;
+document.getElementById("TF").innerHTML= "Is Text 1 is Smaller than Text 2? " + result;
+const person={firstName:"fandogh", lastName:"tut farangi",age:"3.5"};
+document.getElementById("BIO").innerHTML= person.firstName + " is " + person.age + " years old. " ;
+let x=a; let y=b
